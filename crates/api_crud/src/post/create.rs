8414e86106618c1338448dfd93f756af5c8374a0
xaegreal <xaegreal@users.noreply.github.com>
2024-04-03 23:09:00 +0000

    Auto apply NSFW patch to 0.19.4-beta.2

--- crates/api_crud/src/post/create.rs
+++ crates/api_crud/src/post/create.rs
@@ -121,6 +121,13 @@ pub async fn create_post(
     }
   };
 
+  // if the community is NSFW flagged, then flag the post as NSFW no matter what the request data says
+  // not applicable to non-nsfw instances
+  let nsfw = if community.nsfw {
+    true
+  } else {
+    data.nsfw.unwrap_or(true)
+  };
   let post_form = PostInsertForm::builder()
     .name(data.name.trim().to_string())
     .url(url)
@@ -128,7 +135,7 @@ pub async fn create_post(
     .alt_text(data.alt_text.clone())
     .community_id(data.community_id)
     .creator_id(local_user_view.person.id)
-    .nsfw(data.nsfw)
+    .nsfw(Some(nsfw))
     .language_id(language_id)
     .build();
 
