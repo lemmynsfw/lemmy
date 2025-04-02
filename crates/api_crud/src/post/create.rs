e7ea28b9157bc4e0b89c4183c17ed9ada4a13426
xaegit <xaegit@users.noreply.github.com>
2025-04-02 16:32:47 +0000

    Auto apply NSFW patch to 1.0.0-publish-native-binary.7

--- crates/api_crud/src/post/create.rs
+++ crates/api_crud/src/post/create.rs
@@ -85,7 +85,7 @@ pub async fn create_post(
   check_community_user_action(&local_user_view, &community, &mut context.pool()).await?;
 
   // If its an NSFW community, then use that as a default
-  let nsfw = data.nsfw.or(Some(community.nsfw));
+  let nsfw = Some(true);
 
   if community.posting_restricted_to_mods {
     let community_id = data.community_id;
