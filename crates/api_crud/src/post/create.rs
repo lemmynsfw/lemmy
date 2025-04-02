6d3117bc3251e680801974d693e98be9546b2f0d
xaegit <xaegit@users.noreply.github.com>
2025-04-02 16:32:48 +0000

    Auto apply NSFW patch to 0.19.11-beta.0

--- crates/api_crud/src/post/create.rs
+++ crates/api_crud/src/post/create.rs
@@ -1,38 +1,39 @@
+use super::convert_published_time;
+use crate::community_use_pending;
 use activitypub_federation::config::Data;
 use actix_web::web::Json;
 use lemmy_api_common::{
-  build_response::build_post_response,
+  build_response::{build_post_response, send_local_notifs},
   context::LemmyContext,
+  plugins::{plugin_hook_after, plugin_hook_before},
   post::{CreatePost, PostResponse},
   request::generate_post_link_metadata,
   send_activity::SendActivityData,
   utils::{
     check_community_user_action,
+    check_nsfw_allowed,
     get_url_blocklist,
     honeypot_check,
-    local_site_to_slur_regex,
-    mark_post_as_read,
     process_markdown_opt,
+    send_webmention,
+    slur_regex,
   },
 };
 use lemmy_db_schema::{
-  impls::actor_language::default_post_language,
+  impls::actor_language::validate_post_language,
+  newtypes::PostOrCommentId,
   source::{
-    actor_language::CommunityLanguage,
     community::Community,
-    local_site::LocalSite,
-    post::{Post, PostInsertForm, PostLike, PostLikeForm},
+    post::{Post, PostActions, PostInsertForm, PostLikeForm, PostReadForm},
   },
-  traits::{Crud, Likeable},
+  traits::{Crud, Likeable, Readable},
   utils::diesel_url_create,
-  CommunityVisibility,
 };
-use lemmy_db_views::structs::LocalUserView;
-use lemmy_db_views_actor::structs::CommunityModeratorView;
+use lemmy_db_views::structs::{CommunityModeratorView, LocalUserView, SiteView};
 use lemmy_utils::{
   error::{LemmyErrorExt, LemmyErrorType, LemmyResult},
-  spawn_try_task,
   utils::{
+    mention::scrape_text_for_mentions,
     slurs::check_slurs,
     validation::{
       is_url_blocked,
@@ -43,27 +44,23 @@ use lemmy_utils::{
     },
   },
 };
-use tracing::Instrument;
-use url::Url;
-use webmention::{Webmention, WebmentionError};
 
-#[tracing::instrument(skip(context))]
 pub async fn create_post(
   data: Json<CreatePost>,
   context: Data<LemmyContext>,
   local_user_view: LocalUserView,
 ) -> LemmyResult<Json<PostResponse>> {
-  let local_site = LocalSite::read(&mut context.pool()).await?;
-
   honeypot_check(&data.honeypot)?;
+  let local_site = SiteView::read_local(&mut context.pool()).await?.local_site;
 
-  let slur_regex = local_site_to_slur_regex(&local_site);
+  let slur_regex = slur_regex(&context).await?;
   check_slurs(&data.name, &slur_regex)?;
   let url_blocklist = get_url_blocklist(&context).await?;
 
   let body = process_markdown_opt(&data.body, &slur_regex, &url_blocklist, &context).await?;
   let url = diesel_url_create(data.url.as_deref())?;
   let custom_thumbnail = diesel_url_create(data.custom_thumbnail.as_deref())?;
+  check_nsfw_allowed(data.nsfw, Some(&local_site))?;
 
   is_valid_post_title(&data.name)?;
 
@@ -84,72 +81,65 @@ pub async fn create_post(
     is_valid_body_field(body, true)?;
   }
 
-  check_community_user_action(
-    &local_user_view.person,
-    data.community_id,
-    &mut context.pool(),
-  )
-  .await?;
+  let community = Community::read(&mut context.pool(), data.community_id).await?;
+  check_community_user_action(&local_user_view, &community, &mut context.pool()).await?;
 
-  let community_id = data.community_id;
-  let community = Community::read(&mut context.pool(), community_id)
-    .await?
-    .ok_or(LemmyErrorType::CouldntFindCommunity)?;
+  // If its an NSFW community, then use that as a default
+  let nsfw = Some(true);
+
   if community.posting_restricted_to_mods {
     let community_id = data.community_id;
-    let is_mod = CommunityModeratorView::is_community_moderator(
+    CommunityModeratorView::check_is_community_moderator(
       &mut context.pool(),
       community_id,
       local_user_view.local_user.person_id,
     )
     .await?;
-    if !is_mod {
-      Err(LemmyErrorType::OnlyModsCanPostInCommunity)?
-    }
   }
 
-  // Only need to check if language is allowed in case user set it explicitly. When using default
-  // language, it already only returns allowed languages.
-  CommunityLanguage::is_allowed_community_language(
+  let language_id = validate_post_language(
     &mut context.pool(),
     data.language_id,
-    community_id,
+    data.community_id,
+    local_user_view.local_user.id,
   )
   .await?;
 
-  // attempt to set default language if none was provided
-  let language_id = match data.language_id {
-    Some(lid) => Some(lid),
-    None => {
-      default_post_language(
-        &mut context.pool(),
-        community_id,
-        local_user_view.local_user.id,
-      )
-      .await?
-    }
+  let scheduled_publish_time =
+    convert_published_time(data.scheduled_publish_time, &local_user_view, &context).await?;
+  let mut post_form = PostInsertForm {
+    url,
+    body,
+    alt_text: data.alt_text.clone(),
+    nsfw,
+    language_id: Some(language_id),
+    federation_pending: Some(community_use_pending(&community, &context).await),
+    scheduled_publish_time,
+    ..PostInsertForm::new(
+      data.name.trim().to_string(),
+      local_user_view.person.id,
+      data.community_id,
+    )
   };
 
-  let post_form = PostInsertForm::builder()
-    .name(data.name.trim().to_string())
-    .url(url.map(Into::into))
-    .body(body)
-    .alt_text(data.alt_text.clone())
-    .community_id(data.community_id)
-    .creator_id(local_user_view.person.id)
-    .nsfw(data.nsfw)
-    .language_id(language_id)
-    .build();
+  post_form = plugin_hook_before("before_create_local_post", post_form).await?;
 
   let inserted_post = Post::create(&mut context.pool(), &post_form)
     .await
     .with_lemmy_type(LemmyErrorType::CouldntCreatePost)?;
+  plugin_hook_after("after_create_local_post", &inserted_post)?;
 
+  let community_id = community.id;
+  let federate_post = if scheduled_publish_time.is_none() {
+    send_webmention(inserted_post.clone(), community);
+    |post| Some(SendActivityData::CreatePost(post))
+  } else {
+    |_| None
+  };
   generate_post_link_metadata(
     inserted_post.clone(),
     custom_thumbnail.map(Into::into),
-    |post| Some(SendActivityData::CreatePost(post)),
-    Some(local_site),
+    federate_post,
     context.reset_request_count(),
   )
   .await?;
@@ -157,36 +147,24 @@ pub async fn create_post(
   // They like their own post by default
   let person_id = local_user_view.person.id;
   let post_id = inserted_post.id;
-  let like_form = PostLikeForm {
-    post_id,
-    person_id,
-    score: 1,
-  };
+  let like_form = PostLikeForm::new(post_id, person_id, 1);
 
-  PostLike::like(&mut context.pool(), &like_form)
-    .await
-    .with_lemmy_type(LemmyErrorType::CouldntLikePost)?;
+  PostActions::like(&mut context.pool(), &like_form).await?;
 
-  mark_post_as_read(person_id, post_id, &mut context.pool()).await?;
+  // Scan the post body for user mentions, add those rows
+  let mentions = scrape_text_for_mentions(&inserted_post.body.clone().unwrap_or_default());
+  send_local_notifs(
+    mentions,
+    PostOrCommentId::Post(inserted_post.id),
+    &local_user_view.person,
+    true,
+    &context,
+    Some(&local_user_view),
+  )
+  .await?;
 
-  if let Some(url) = inserted_post.url.clone() {
-    if community.visibility == CommunityVisibility::Public {
-      spawn_try_task(async move {
-        let mut webmention =
-          Webmention::new::<Url>(inserted_post.ap_id.clone().into(), url.clone().into())?;
-        webmention.set_checked(true);
-        match webmention
-          .send()
-          .instrument(tracing::info_span!("Sending webmention"))
-          .await
-        {
-          Err(WebmentionError::NoEndpointDiscovered(_)) => Ok(()),
-          Ok(_) => Ok(()),
-          Err(e) => Err(e).with_lemmy_type(LemmyErrorType::CouldntSendWebmention),
-        }
-      });
-    }
-  };
+  let read_form = PostReadForm::new(post_id, person_id);
+  PostActions::mark_as_read(&mut context.pool(), &read_form).await?;
 
   build_post_response(&context, community_id, local_user_view, post_id).await
 }
